553d3576fa1b51eaf0101b5d7fa9fa4875dc5169
zhang12247 <zhangjia12247268@163.com>
2019-04-18 15:21:24 +0800

    添加观察者模式（完成）

---
+++ 观察者模式/理解观察者模式.py
@@ -0,0 +1,33 @@
+class Subject:
+    def __init__(self):
+        self.__observers = []
+
+    def register(self, observer):
+        self.__observers.append(observer)
+
+    def notifyAll(self, *args, **kwargs):
+        for observer in self.__observers:
+            observer.notify(self, *args, **kwargs)
+
+
+class Observerl:
+    def __init__(self, subject):
+        subject.register(self)
+
+    def notify(self, subject, *args):
+        print(type(self).__name__, '::Got', args, 'From', subject)
+
+
+class Observer2:
+    def __init__(self, subject):
+        subject.register(self)
+
+    def notify(self, subject, *args):
+        print(type(self).__name__, '::Got', args, 'From', subject)
+
+
+if __name__ == '__main__':
+    subject = Subject()
+    observer1 = Observerl(subject)
+    observer2 = Observer2(subject)
+    subject.notifyAll('notification')
